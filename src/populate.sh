37ec156703f286cc4e7e70be731c9f47b3bc92e7
Henry Monge-Camacho <henrymonge@gmail.com>
2025-03-18 06:56:43 -0700

    added autoreconf line for GLU in populate.sh

--- src/populate.sh
+++ src/populate.sh
@@ -9,6 +9,14 @@ git clone --recursive -b devel https://github.com/usqcd-software/qdpxx.git
 git clone -b develop https://github.com/lattice/quda.git
 
 git clone --recursive -b devel https://github.com/jeffersonlab/chroma.git
+cd chroma
+git checkout 3ae4e0d
+cd ../
 
-#git clone -b release/14.x https://github.com/llvm/llvm-project
+git clone git@github.com:RJHudspith/GLU.git
+cd GLU
+autoreconf -f -i
+cd ../
+
+git clone --recursive -b qedm https://github.com/callat-qcd/lalibe.git
 
